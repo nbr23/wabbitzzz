ef5fe833cec51822ce6c6da9bb4c1b2739efe473
Mike Chevett <mchevett@gmail.com>
2022-09-18 22:32:24 -0400

    make the autoDelete queue names less ugly

--- queue.js
+++ queue.js
@@ -1,4 +1,4 @@
-var uuid = require('ezuuid');
+var uuid = require('short-id').generate
 var _ = require('lodash');
 var defaultExchangePublish = require('./default-exchange-publish');
 var Promise = require('bluebird');
