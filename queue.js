ee278ad2dafe88f6837a546f6863d83ea40cc594
Max <max@23.tf>
2024-11-14 16:54:08 -0500

    fix '#' label handling (#31)

--- queue.js
+++ queue.js
@@ -51,6 +51,7 @@ function _patternToMatcher(pattern) {
 	const regexString = '^' +
 		pattern.replace(/[.]/g, '\\.') // escape dots
 			.replace(/\*/g, '([^.]+)') // single word wildcard
+			.replace(/^#$/, '([^.]+[.]?)*') // multi word wildcard on its own
 			.replace(/^#\\./, '([^.]+[.])*') // multi word wildcard at start of key
 			.replace(/\\.#$/, '([.][^.]+)*') // multi word wildcard at end of key
 			.replace(/\\.#\\./g, '(([.].*[.])*|[.])') // multi word wildcard within key
